7239d0d38952f03113c1c4d2c489f48e566cdc66
Simon Overell <soverell@gmail.com>
2021-03-10 14:32:15 +0000

    Switch to Parcel CLI
    This provides no benefit over the parcel CLI, so switching to that for now.

--- file-upload/scripts/upload-oss.sh
+++ file-upload/scripts/upload-oss.sh
@@ -14,9 +14,9 @@ head -n 80000 oss-textdb/Paragraphs.txt > oss-textdb/Paragraphs.txt.chunk1
 tail -n +80001 oss-textdb/Paragraphs.txt > oss-textdb/Paragraphs.txt.chunk2
 
 #Upload
-npm run-script run -- --sourcePath oss-textdb/Chapters.txt --title "OSS Chapters"
-npm run-script run -- --sourcePath oss-textdb/Characters.txt --title "OSS Characters"
-npm run-script run -- --sourcePath oss-textdb/Paragraphs.txt.chunk1 --title "OSS Paragraphs chunk1"
-npm run-script run -- --sourcePath oss-textdb/Paragraphs.txt.chunk2 --title "OSS Paragraphs chunk2"
-npm run-script run -- --sourcePath oss-textdb/WordForms.txt --title "OSS WordForms"
-npm run-script run -- --sourcePath oss-textdb/Works.txt --title "OSS Works"
+parcel upload --fromPath oss-textdb/Chapters.txt --datasetTitle "OSS Chapters"
+parcel upload --fromPath oss-textdb/Characters.txt --datasetTitle "OSS Characters"
+parcel upload --fromPath oss-textdb/Paragraphs.txt.chunk1 --datasetTitle "OSS Paragraphs chunk1"
+parcel upload --fromPath oss-textdb/Paragraphs.txt.chunk2 --datasetTitle "OSS Paragraphs chunk2"
+parcel upload --fromPath oss-textdb/WordForms.txt --datasetTitle "OSS WordForms"
+parcel upload --fromPath oss-textdb/Works.txt --datasetTitle "OSS Works"
