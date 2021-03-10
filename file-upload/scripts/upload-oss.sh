09981a6deff9e2666eaab71af5576574adc07ecb
Simon Overell <soverell@gmail.com>
2021-03-10 12:00:12 +0000

    Add chunking to example

--- file-upload/scripts/upload-oss.sh
+++ file-upload/scripts/upload-oss.sh
@@ -5,11 +5,18 @@
 
 # Download and unzip the delimimeted text files of OSS
 wget https://www.opensourceshakespeare.org/downloads/oss-textdb.zip
-unzip oss-textdb.zip
+mkdir oss-textdb
+unzip -d oss-textdb oss-textdb.zip
+
+#TODO: Parcel appears to have a default 10Meg file size limit. Follow up on how
+# to upload larger files however in the meantime chunk down.
+head -n 80000 oss-textdb/Paragraphs.txt > oss-textdb/Paragraphs.txt.chunk1
+tail -n +80001 oss-textdb/Paragraphs.txt > oss-textdb/Paragraphs.txt.chunk2
 
 #Upload
 npm run-script run -- --sourcePath oss-textdb/Chapters.txt --title "OSS Chapters"
 npm run-script run -- --sourcePath oss-textdb/Characters.txt --title "OSS Characters"
-npm run-script run -- --sourcePath oss-textdb/Paragraphs.txt --title "OSS Paragraphs"
+npm run-script run -- --sourcePath oss-textdb/Paragraphs.txt.chunk1 --title "OSS Paragraphs chunk1"
+npm run-script run -- --sourcePath oss-textdb/Paragraphs.txt.chunk2 --title "OSS Paragraphs chunk2"
 npm run-script run -- --sourcePath oss-textdb/WordForms.txt --title "OSS WordForms"
 npm run-script run -- --sourcePath oss-textdb/Works.txt --title "OSS Works"
