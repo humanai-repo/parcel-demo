7cd21bb60500a0545dd567896bca92b10742d874
Simon Overell <soverell@gmail.com>
2021-03-15 14:30:11 +0000

    Add extractor script

---
+++ extractor-shakespeare/README.md
@@ -0,0 +1,40 @@
+# Extract per play data from Shakespeare plays
+
+Extracts per play data from shakespeare plays. This is in preparation for an
+untrusted process to intereact with the data.
+
+Assumes you have followed [Upload Shakespeare Summary](../file-upload/scropts/upload-oss.sh).
+
+## Build
+
+```bash
+npm install tslib
+npm run-script build
+```
+
+## Run
+
+The address of the Shakespeare Paragraph Chunks is printed when running the upload script.
+They start "0x...".
+Alternatively you can run the [dataset-list](../dataset-list/) script and read it off.
+
+```bash
+export OASIS_CLIENT_ID="{OASIS_CLIENT_ID}"
+export OASIS_API_PRIVATE_KEY={OASIS_API_PRIVATE_KEY}
+export SHAKESPEARE_PARA1_ADDRESS={SHAKESPEARE_PARA1_ADDRESS}
+export SHAKESPEARE_PARA2_ADDRESS={SHAKESPEARE_PARA2_ADDRESS}
+export SHAKESPEARE_N_PLAYS=43
+npm run-script run
+```
+
+Instructions on reading the output can be found in the [Dataset List Readme](../dataset-list/).
+
+## Debugging tips
+
+The docker command executed by the job can be run locally with:
+
+```bash
+docker run -v $DATA_DIR:/parcel humansimon/csv_extractor \
+  csv_extractor -i /parcel/Paragraphs.txt.chunk1 -i /parcel/Paragraphs.txt.chunk2 \
+  -c PlainText --id WorkID -n 43 -p /parcel/ExtratedParagraphs.csv
+```
---
+++ extractor-shakespeare/src/main.ts
@@ -0,0 +1,68 @@
+import * as Parcel from '@oasislabs/parcel-sdk';
+
+// #region snippet-config
+const configParams = Parcel.Config.paramsFromEnv();
+const config = new Parcel.Config(configParams);
+// #endregion snippet-config
+
+async function main() {
+    console.log('Here we go...');
+
+    const identity = await config.getTokenIdentity();
+    const dispatcher = await Parcel.Dispatcher.connect(config.dispatcherAddress, identity, config);
+
+    // read the OSS Paragraphs chunks address from the
+    // SHAKESPEARE_PARA1_ADDRESS and SHAKESPEARE_PARA2_ADDRESS env variables.
+    // Read the number of plays from the SHAKESPEARE_N_PLAYS env variable.
+    // TODO: Refactor such that the number of input chunks is not hardcoded.
+    const shakespeareParaAddress1 = process.env.SHAKESPEARE_PARA1_ADDRESS;
+    const shakespeareParaAddress2 = process.env.SHAKESPEARE_PARA2_ADDRESS;
+    const shakespeareNPlays = parseInt(process.env.SHAKESPEARE_N_PLAYS);
+
+    const fileNamePrefix = "ExtratedParagraphs"
+
+    var outputDatasets = []
+    for (var i = 0; i < shakespeareNPlays; i++) {
+        outputDatasets.push({ mountPath: `${fileNamePrefix}-${i}.csv`, owner: identity })
+    }
+
+
+    // Submit the job.
+    // #region snippet-submit-job
+    const jobRequest = {
+        name: 'extractor-shakespeare',
+        dockerImage: 'humansimon/csv-extractor',
+        inputDatasets: [{ mountPath: 'Paragraphs.txt.chunk1', address: new Parcel.Address(shakespeareParaAddress1)},
+                        { mountPath: 'Paragraphs.txt.chunk2', address: new Parcel.Address(shakespeareParaAddress2)}],
+        outputDatasets: outputDatasets,
+        cmd: [
+            'csv_extractor',
+            '-i',
+            '/parcel/data/in/Paragraphs.txt.chunk1',
+            '-i',
+            '/parcel/data/in/Paragraphs.txt.chunk2',
+            '-c',
+            'PlainText',
+            '--id',
+            'WorkID',
+            '-n',
+            `${shakespeareNPlays}`,
+            '-p',
+            `/parcel/data/out/${fileNamePrefix}`,
+        ],
+    };
+
+    const jobId = await dispatcher.submitJob({ job: jobRequest });
+    // #endregion snippet-submit-job
+    console.log(`Job ${Parcel.utils.encodeHex(jobId)} submitted.`);
+
+    // Wait for job completion.
+    const job = await dispatcher.getCompletedJobInfo(jobId);
+    if (job.status instanceof Parcel.JobCompletionStatus.Success) {
+        console.log('Job completed successfully!');
+    } else {
+        console.log('Job failed!', job.info);
+    }
+}
+
+main();
