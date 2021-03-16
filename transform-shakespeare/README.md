154791f609080f575be656ec986f44d6aa248ddf
Simon Overell <soverell@gmail.com>
2021-03-16 20:45:05 +0000

    Move transformer to docker, add readme and build files.

---
+++ transform-shakespeare/README.md
@@ -0,0 +1,45 @@
+# Applies a transform to a Shakespeare play
+
+Applies a transform to the text of a Shakespeare plays extracted with
+[extractor](../extractor-shakespeare). The specific transformation performed
+is a word-count.
+
+Computation is performed inside a csvkit docker container which is likely
+over-kill however works for the technology demonstration.
+
+## Build
+
+```bash
+npm install tslib
+npm install ts-command-line-args
+npm run-script build
+```
+
+## Run
+
+A file of input addresses, one address per line, is passed in as a command line arg.
+An analogous output file is written.
+
+```bash
+npm run-script run -- -h
+
+Options
+
+  -a, --inputAddresses string    Path to a list of input addresses, one address per line.                 
+  -o, --outputAddresses string   Optional path to write a list of output addresses, one address per line. 
+  -h, --help                     Prints this usage guide 
+```
+
+```bash
+export OASIS_CLIENT_ID="{OASIS_CLIENT_ID}"
+export OASIS_API_PRIVATE_KEY={OASIS_API_PRIVATE_KEY}
+npm run-script run -- -a working-data/input.txt -o working-data/output.txt
+```
+
+## Debugging tips
+
+The docker command executed by the job can be run locally with:
+
+```bash
+# TODO
+```
--- transform-shakespeare/src/main.ts
+++ transform-shakespeare/src/main.ts
@@ -1,8 +1,33 @@
 import * as Parcel from '@oasislabs/parcel-sdk';
+import { parse } from 'ts-command-line-args';
+import * as fs from 'fs';
+import * as rd from 'readline';
+import * as process from 'process';
 
 // #region snippet-config
 const configParams = Parcel.Config.paramsFromEnv();
 const config = new Parcel.Config(configParams);
+
+interface IOArguments {
+    // TODO: Make input addresses optional and allow a single input address
+    // to be passed instead of a path to a file of addresses
+    inputAddresses: string;
+    outputAddresses?: string;
+    help?: boolean;
+}
+
+export const args = parse<IOArguments>(
+    {
+        inputAddresses: {type: String, alias: 'a', optional: true,
+            description: 'Path to a list of input addresses, one address per line.'},
+        outputAddresses: {type: String, alias: 'o', optional: true,
+            description: 'Optional path to write a list of output addresses, one address per line.'},
+        help: { type: Boolean, optional: true, alias: 'h', description: 'Prints this usage guide' },
+    },
+    {
+        helpArg: 'help',
+    },
+);
 // #endregion snippet-config
 
 async function main() {
@@ -11,60 +36,65 @@ async function main() {
     const identity = await config.getTokenIdentity();
     const dispatcher = await Parcel.Dispatcher.connect(config.dispatcherAddress, identity, config);
 
-    //TODO:
-    // Take as a command line argument either a file containing a list of
-    // addresses or a single address.
-    // Take an optional command line argument to write a list of ouput addresses.
+    // #region snippit-read-input-addresses
+    let reader = rd.createInterface(fs.createReadStream(args.inputAddresses));
+    //TODO: filter blank lines
+    const inputAddresses = fs.readFileSync(args.inputAddresses, 'ascii').split("\n").filter(l => l !== '');
+    // #endregion snippit-read-input-addresses
 
-    const inputAddress = ""
+    // #region snippet-create-job-requests
+    let jobRequests = inputAddresses.map(
+        (i) => {
+            console.log(i);
+            return {
+                name: 'transform-shakespeare',
+                //dockerImage: 'patagonagmbh/csvkit:1.0.3',
+                dockerImage: 'humansimon/simple-transform',
+                inputDatasets: [{ mountPath: 'input.csv', address: new Parcel.Address(i)},],
+                outputDatasets: [{ mountPath: 'output.csv', owner: identity }],
+                //echo "words,index" > /data/ouput.csv ; csvcut -c PlainText data/paragraphs-0.csv | tail -n+2 | wc -w  | sed 's/$/,1/' >> /data/ouput.csv
+                cmd: [
+                'simple_transform',
+                '-i',
+                '/parcel/data/in/input.csv',
+                '-c',
+                'PlainText',
+                '--index',
+                i,
+                '-o',
+                '/parcel/data/out/output.csv',
+                ],
+            };
+        }
+    );
+    // #endregion snippet-create-job-requests
 
-    // TODO: Create a for loop building a set of Job Requests
+    // Submit the jobs.
+    
+    let outputAddresses: string[] = [];
+    // TODO: This submits each job sequentially -- swap to ascync
+    for (let jobRequest of jobRequests) {
+        const jobId = await dispatcher.submitJob({ job: jobRequest });
+        // #endregion snippet-submit-job
+        console.log(`Job ${Parcel.utils.encodeHex(jobId)} submitted.`);
 
-    // Submit the job.
-    // #region snippet-submit-job
-    const jobRequest = {
-        name: 'extractor-shakespeare',
-        dockerImage: 'humansimon/csv-extractor',
-        inputDatasets: [{ mountPath: 'Paragraphs.txt.chunk1', address: new Parcel.Address(inputAddress)},],
-        outputDatasets: outputDatasets,
-        //echo "words,index" > /data/ouput.csv ; csvcut -c PlainText data/paragraphs-0.csv | tail -n+2 | wc -w  | sed 's/$/,1/' >> /data/ouput.csv
-        cmd: [
-            "echo",
-            "\"words,index\"",
-            ">",
-            "/data/ouput.csv",
-            ";",
-            "csvcut",
-            "-c",
-            "PlainText",
-            "data/paragraphs-0.csv",
-            "|",
-            "tail",
-            "-n+2",
-            "|",
-            "wc",
-            "-w",
-            "|",
-            "sed",
-            "'s/$/,1/'",
-            ">>",
-            "/data/ouput.csv",
-        ],
-    };
-
-    const jobId = await dispatcher.submitJob({ job: jobRequest });
-    // #endregion snippet-submit-job
-    console.log(`Job ${Parcel.utils.encodeHex(jobId)} submitted.`);
-
-    // Wait for job completion.
-    const job = await dispatcher.getCompletedJobInfo(jobId);
-    if (job.status instanceof Parcel.JobCompletionStatus.Success) {
-        console.log('Job completed successfully!');
-    } else {
-        console.log('Job failed!', job.info);
-    }
-
+        // Wait for job completion.
+        const job = await dispatcher.getCompletedJobInfo(jobId);
+        if (job.status instanceof Parcel.JobCompletionStatus.Success) {
+            console.log('Job completed successfully!');
+        } else {
+            console.log('Job failed!', job.info);
+            console.log('Exiting with falure status code 1');
+            return process.exit(1);
+        }
+        outputAddresses = outputAddresses.concat(job.outputs.map((o) => {return o.address.hex;}));
+     }
     //An example reading output addresses
+
+    // Write the out addresses to the output file if set
+    if(args.outputAddresses) {
+        fs.writeFileSync(args.outputAddresses, outputAddresses.join("\n"));
+    }
 }
 
 main();
