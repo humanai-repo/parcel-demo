3c862046fedfbafb1e5267ecbaa4a518ad0abed8
Simon Overell <soverell@gmail.com>
2021-03-22 18:25:43 +0000

    Add merge step

--- dp-shakespeare/src/main.ts
+++ dp-shakespeare/src/main.ts
@@ -1,4 +1,4 @@
-import Parcel, {InputDocumentSpec, OutputDocumentSpec, Job, JobSpec, JobPhase, DocumentId} from '@oasislabs/parcel';
+import Parcel, {InputDocumentSpec, OutputDocumentSpec, Job, JobSpec, JobPhase, DocumentId, IdentityId} from '@oasislabs/parcel';
 import { parse } from 'ts-command-line-args';
 import * as fs from 'fs';
 import * as process from 'process';
@@ -6,6 +6,8 @@ import * as process from 'process';
 // #region snippet-config
 const clientId = process.env.PARCEL_CLIENT_ID ?? '';
 const privateKey = JSON.parse(process.env.OASIS_API_PRIVATE_KEY ?? '');
+
+const maxInputFilesPerJob = 10;
 
 interface IOArguments {
     // TODO: Make input addresses optional and allow a single input address
@@ -29,6 +31,73 @@ export const args = parse<IOArguments>(
 );
 // #endregion snippet-config
 
+async function merge(inputAddresses : string [], identity : IdentityId, parcel : Parcel) {
+    // Split input into chunks
+    let inputChunks : string [][] = [];
+    for(let i = 0; i < inputAddresses.length; i+= maxInputFilesPerJob) {
+        inputChunks.push(inputAddresses.slice(i,i+maxInputFilesPerJob));
+    }
+    let inputFileNamePrefix = "InputChunk";
+
+    // Turn chunks into jobSpecs
+    let jobSpecs : JobSpec [] = [];
+    for(let inputChunk of inputChunks) {
+        let inputDocuments : InputDocumentSpec[] = [];
+
+        let cmd = [
+            'csv_merger',
+            '-o',
+            '/parcel/data/out/output.txt',
+        ]
+
+        for(let i of inputChunk) {
+            let fileName = `${inputFileNamePrefix}-${i}.txt`;
+            let filePath = `/parcel/data/in/${fileName}`;
+            inputDocuments.push({ mountPath: fileName, id: i as DocumentId});
+            cmd.push('-i', filePath);
+        }
+
+        console.log(cmd.join(" "));
+
+        // Submit the job.
+        // #region snippet-submit-job
+        const jobSpec: JobSpec = {
+            name: 'csv-merger',
+            image: 'humansimon/csv-merger',
+            inputDocuments: inputDocuments,
+            outputDocuments: [{ mountPath: 'output.txt', owner: identity }],
+            cmd: cmd
+        };
+        jobSpecs.push(jobSpec);
+    }
+
+   let outputAddresses: string[] = [];
+    // TODO: This submits each job sequentially -- swap to ascync
+    for (let jobSpec of jobSpecs) {
+        let jobId = (await parcel.submitJob(jobSpec)).id;
+        // #endregion snippet-submit-job
+        console.log(`Job ${jobId} submitted.`);
+
+        // Wait for job completion.
+        let job: Job;
+        do {
+          await new Promise((resolve) => setTimeout(resolve, 5000)); // eslint-disable-line no-promise-executor-return
+          job = await parcel.getJob(jobId);
+          console.log(`Job status is ${JSON.stringify(job.status)}`);
+        } while (job.status.phase === JobPhase.PENDING || job.status.phase === JobPhase.RUNNING);
+
+        if (job.status.phase === JobPhase.SUCCEEDED) {
+            console.log('Job completed successfully!');
+        } else {
+            console.log('Job failed!');
+            console.log('Exiting with falure status code 1');
+            return process.exit(1);
+        }
+        outputAddresses = outputAddresses.concat(job.status.outputDocuments.map((o) => {return o.id;}));
+     }
+    return outputAddresses;
+}
+
 async function main() {
     console.log('Here we go...');
 
@@ -38,12 +107,17 @@ async function main() {
     });
     const identity =  (await parcel.getCurrentIdentity()).id;
 
-    const inputAddresses = fs.readFileSync(args.inputAddresses || '', 'ascii').split("\n").filter(l => l !== '');
+    let inputAddresses = fs.readFileSync(args.inputAddresses || '', 'ascii').split("\n").filter(l => l !== '');
     
-    const inputFileNamePrefix = "InputChunk";
+    // Parcel 0.1.9 errors with > 10 input addresses, so we run multiple rounds of merging.
+    while(inputAddresses.length > maxInputFilesPerJob) {
+        inputAddresses = await merge(inputAddresses, identity, parcel);
+    }
 
-    var inputDocuments : InputDocumentSpec[] = [];
-    var inputCmd : string[] = [];
+    let inputFileNamePrefix = "InputChunk";
+
+    let inputDocuments : InputDocumentSpec[] = [];
+    let inputCmd : string[] = [];
     for(let i in inputAddresses) {
         let fileName = `${inputFileNamePrefix}-${i}.txt`;
         let filePath = `/parcel/data/in/${fileName}`;
@@ -51,7 +125,7 @@ async function main() {
         inputCmd.push('-i', filePath);
     }
 
-    var cmd = [
+    let cmd = [
         'pydp-cli',
         '-c',
         'words',
