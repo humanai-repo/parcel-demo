5878b0a96e36aae55b5fbc58d1bc8a52c5988cd9
Simon Overell <soverell@gmail.com>
2021-03-23 15:56:18 +0000

    Minor fixes during testing

--- eta-shakespeare/scripts/eta-shakespeare.sh
+++ eta-shakespeare/scripts/eta-shakespeare.sh
@@ -7,8 +7,7 @@ WORKING_DIR=$HOME_DIR/working-data
 # Paths for intemediate data
 PER_ENTITY_INPUT_ADDRESSES=$WORKING_DIR/per-entity-input-addresses.txt
 PER_ENTITY_OUTPUT_ADDRESSES=$WORKING_DIR/per-entity-output-addresses.txt
-OUTPUT_ADDRESS=$WORKING_DIR/ouput_address.txt
-OUPUT=$WORKING_DIR/output.txt
+OUTPUT_ADDRESS=$WORKING_DIR/output_address.txt
 # Read the command lines
 INPUT_ADDRESSES=$1
 N_ENTITIES=$2
@@ -19,7 +18,7 @@ echo "Reading from input addresses in $INPUT_ADDRESSES"
 cd $HOME_DIR/../extractor-shakespeare
 npm run start -- -a $INPUT_ADDRESSES -n $N_ENTITIES -o $PER_ENTITY_INPUT_ADDRESSES
 STATUS=$?
-echo "Extract ouput addresses $PER_ENTITY_INPUT_ADDRESSES"
+echo "Extract output addresses $PER_ENTITY_INPUT_ADDRESSES"
 
 #If it errored then bail
 if test $status -neq 0
@@ -32,7 +31,7 @@ echo "TRANSFORM"
 cd $HOME_DIR/../transform-shakespeare 
 npm run start -- -a $PER_ENTITY_INPUT_ADDRESSES -o $PER_ENTITY_OUTPUT_ADDRESSES
 STATUS=$?
-echo "Transform ouput addresses $PER_ENTITY_OUTPUT_ADDRESSES"
+echo "Transform output addresses $PER_ENTITY_OUTPUT_ADDRESSES"
 
 #If it errored then bail
 if test $status -neq 0
@@ -43,16 +42,20 @@ fi
 echo "AGGREGATE"
 # Run the aggregator
 cd $HOME_DIR/../dp-shakespeare
-npm run start -- -a $PER_ENTITY_INPUT_ADDRESSES -o $OUTPUT_ADDRESS
+npm run start -- -a $PER_ENTITY_OUTPUT_ADDRESSES -o $OUTPUT_ADDRESS
 STATUS=$?
 if test $status -neq 0
 then
 	exit 1
 fi
-echo "Aggregate ouput address $OUTPUT_ADDRESS"
+echo "Aggregate output address $OUTPUT_ADDRESS"
 
 echo "DOWNLOAD OUTPUT"
 # Download the output
 cd $HOME_DIR/../data-utils
 npm run start -- -a $OUTPUT_ADDRESS -p $HOME_DIR/working-data
+OUTPUT=`ls -t1 | head -n1`
 
+echo "Output file $OUTPUT"
+
+cat $OUTPUT
